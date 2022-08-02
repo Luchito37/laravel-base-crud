74a7a3637fb8faf8681cefa133c5735171ce4b7f
Luchito37 <lucamatteotti01@gmail.com>
2022-08-02 18:06:38 +0200

    creato la nav

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('page_title', 'Home Page')
+
+@section('page_content')
+        <div>
+            <div class="container d-flex vh-100 align-items-center justify-content-center">
+                <div class="row ">
+                    <div class="col">
+                        <h1 class=" font-title">Benvenuto nel mondo BOOLI<span class="text-success">CS</span></h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+@endsection
